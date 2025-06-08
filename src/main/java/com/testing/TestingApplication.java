c0eccbb74b4a6fba48ae21b9a4b6b209bd78aa74
Pawan Pumar <itsmekr.info@gmail.com>
2025-06-08 13:20:41 +0530

    changes port

--- src/main/java/com/testing/TestingApplication.java
+++ src/main/java/com/testing/TestingApplication.java
@@ -2,12 +2,19 @@ package com.testing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TestingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestingApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public static String getHello() {
+        return "Hello, World!";
+    }
 }
